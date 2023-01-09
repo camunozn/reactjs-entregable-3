73e4803f553770af1569873f4195a0da4f2445d9
camunozn <camunozn89@gmail.com>
2023-01-09 12:36:59 -0500

    added header element

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
 import { useState } from 'react';
+import headerImg from './assets/img/header-img.png';
+import headerTitle from './assets/img/header-title.png';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header className="header">
+        <img
+          className="header-img"
+          src={headerImg}
+          alt="Rick and Morty header image"
+        />
+        <img
+          className="header-title"
+          src={headerTitle}
+          alt="Rick and Morty header title"
+        />
+      </header>
+      <main></main>
+    </div>
+  );
 }
 
 export default App;
